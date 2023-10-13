d89e0e3cf9500bdac3eca0ad26b3c3c4452e352f
ervanervan <ervanervan040@gmail.com>
2023-10-13 22:56:35 +0700

    Finished creating the Hero Goout

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -12,54 +12,60 @@ export default function () {
         <div className="md:px-8 px-4 flex gap-4 items-center justify-between">
           <img src={iconMap} alt="" />
           <div>
-            <select
-              id="location"
-              name="location"
-              autoComplete="location-name"
-              className="w-full rounded-md py-2 md:py-1 text-textNavy text-sm xl:text-base appearance-none font-medium ring-bgPrimary focus:ring-0 focus:ring-inset focus:ring-bgPrimary"
-            >
-              <option>NTB, Lombok</option>
-              <option>Ubud, Bali</option>
-              <option>Bali, Nusa Penida</option>
-              <option>Sumatera Utara, Danau Toba</option>
-              <option>Jawa Tengah, Candi Borobudur</option>
-            </select>
-            <label className="text-xs text-textGray">
-              Select the locations
-            </label>
+            <form action="">
+              <select
+                id="loc"
+                name="selectloc"
+                className="w-full rounded-md py-2 md:py-1 text-textNavy text-sm xl:text-base appearance-none font-medium ring-bgPrimary focus:ring-0 focus:ring-inset focus:ring-bgPrimary"
+              >
+                <option>NTB, Lombok</option>
+                <option>Ubud, Bali</option>
+                <option>Bali, Nusa Penida</option>
+                <option>Sumatera Utara, Danau Toba</option>
+                <option>Jawa Tengah, Candi Borobudur</option>
+              </select>
+              <label htmlFor="loc" className="text-xs text-textGray">
+                Select the locations
+              </label>
+            </form>
           </div>
           <img src={iconGps} alt="" />
         </div>
         <div className="md:px-8 px-4 flex gap-4 items-center justify-between">
           <img src={iconCalendar} alt="" />
-          <div className="">
+          <div>
             <input
-              placeholder="Select a date"
+              id="flydate"
               type="date"
               className="w-full rounded-md py-2 md:py-1 text-textNavy text-sm xl:text-base font-medium ring-bgPrimary focus:ring-0 focus:ring-inset focus:ring-bgPrimary"
             />
-            <label className="text-xs text-textGray">Choose Flight Date</label>
+            <label htmlFor="flydate" className="text-xs text-textGray">
+              Choose Flight Date
+            </label>
           </div>
           <img src={iconGps} alt="" />
         </div>
         <div className="md:px-8 px-4 flex gap-4 items-center justify-between">
           <img src={iconChair} alt="" />
-          <div className="">
-            <select
-              id="flighttype"
-              name="flighttype"
-              autoComplete="flight-type"
-              className="w-full rounded-md py-2 md:py-1 text-textNavy text-sm xl:text-base appearance-none font-medium ring-bgPrimary focus:ring-0 focus:ring-inset focus:ring-bgPrimary"
-            >
-              <option>Economy</option>
-              <option>Basic Economy</option>
-              <option>Premium</option>
-              <option>Premium Economy</option>
-              <option>Business</option>
-              <option>First Suites</option>
-              <option>First</option>
-            </select>
-            <label className="text-xs text-textGray">Choose Flight type</label>
+          <div>
+            <form action="">
+              <select
+                id="flytp"
+                name="typefight"
+                className="w-full rounded-md py-2 md:py-1 text-textNavy text-sm xl:text-base appearance-none font-medium ring-bgPrimary focus:ring-0 focus:ring-inset focus:ring-bgPrimary"
+              >
+                <option>Economy</option>
+                <option>Basic Economy</option>
+                <option>Premium</option>
+                <option>Premium Economy</option>
+                <option>Business</option>
+                <option>First Suites</option>
+                <option>First</option>
+              </select>
+              <label htmlFor="flytp" className="text-xs text-textGray">
+                Choose Flight type
+              </label>
+            </form>
           </div>
           <img src={iconGps} alt="" />
         </div>
